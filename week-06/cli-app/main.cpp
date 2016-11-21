21f16fcdee6472c47a9bfacafd98feab3654209a
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-11-21 19:03:24 +0100

    feat(list): working with all flags

--- week-06/cli-app/main.cpp
+++ week-06/cli-app/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
-  std::cout << "CLI Todo application" << std::endl;
-  std::cout << "====================" << std::endl;
-  std::cout << std::endl;
-  std::cout << "Command line arguments:" << std::endl;
-  std::cout << " -l\tlist\t\tLists all the tasks" << std::endl;
-  std::cout << " -a\tadd\t\tAdds a new task" << std::endl;
-  std::cout << " -r\tremove\t\tRemoves a task" << std::endl;
-  std::cout << " -c\tcheck\t\tCompletes a task" << std::endl;
+  if (argc == 1) {
+    std::cout << "CLI Todo application" << std::endl;
+    std::cout << "====================" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Command line arguments:" << std::endl;
+    std::cout << " -l\tlist\t\tLists all the tasks" << std::endl;
+    std::cout << " -a\tadd\t\tAdds a new task" << std::endl;
+    std::cout << " -r\tremove\t\tRemoves a task" << std::endl;
+    std::cout << " -c\tcheck\t\tCompletes a task" << std::endl;
+  } else {
+    std::ifstream db_file(".db.txt");
+    std::string line;
+    int i = 1;
+    while (getline(db_file, line)) {
+      std::cout << i << " - " << line << std::endl;
+      i++;
+    }
+  }
   return 0;
 }
