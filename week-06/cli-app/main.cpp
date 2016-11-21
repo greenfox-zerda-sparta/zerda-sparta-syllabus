fc591a008f7ebcef593cf4366a7ab61b6e82a047
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2016-11-21 20:16:33 +0100

    chore(git): ignore db file

--- week-06/cli-app/main.cpp
+++ week-06/cli-app/main.cpp
@@ -1,24 +1,7 @@
-#include <iostream>
-#include <fstream>
+#include "todo-app.hpp"
 
 int main(int argc, char* argv[]) {
-  if (argc == 1) {
-    std::cout << "CLI Todo application" << std::endl;
-    std::cout << "====================" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Command line arguments:" << std::endl;
-    std::cout << " -l\tlist\t\tLists all the tasks" << std::endl;
-    std::cout << " -a\tadd\t\tAdds a new task" << std::endl;
-    std::cout << " -r\tremove\t\tRemoves a task" << std::endl;
-    std::cout << " -c\tcheck\t\tCompletes a task" << std::endl;
-  } else {
-    std::ifstream db_file(".db.txt");
-    std::string line;
-    int i = 1;
-    while (getline(db_file, line)) {
-      std::cout << i << " - " << line << std::endl;
-      i++;
-    }
-  }
+  TodoApp app;
+  app.run(argc, argv);
   return 0;
 }
